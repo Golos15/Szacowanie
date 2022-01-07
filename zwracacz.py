fa2f060ba0fd9a70b2dbb8bc48175ed3f6d751ce
Kornel <kornel272@gmail.com>
2022-01-08 00:30:53 +0100

    wczytywanie listy z Excela, liczenie średniej ważonej

--- zwracacz.py
+++ zwracacz.py
@@ -1,4 +1,4 @@
-import wynik_liczenia
+from wynik_liczenia import wynik_liczenia
 
 def aproksymujLicznik(listaOtwarta):
     wiersze_puste = list(filter(lambda item: item.odczyt == None, listaOtwarta))
@@ -7,9 +7,12 @@ def aproksymujLicznik(listaOtwarta):
     for wiersz_uzytkownika in wiersze_puste:
         nr_uzytkownika = wiersz_uzytkownika.nr_uzytkownika
         podzielniki_uzytkownika = list(filter(lambda item: item.nr_uzytkownika == nr_uzytkownika, listaOtwarta))
-        wiersze_niepuste = list(filter(lambda item: item.odczyt != None, listaOtwarta))
-        srednia_wazona = sum(c.odczyt for c in wiersze_niepuste)
-        lista_wynikow.append(wynik_liczenia(nr_uzytkownika, wiersz_uzytkownika.numer_podz, wiersz_uzytkownika.wspolczynnik, srednia_wazona, wiersz_uzytkownika.uwagi))
+        wiersze_niepuste = list(filter(lambda item: item.odczyt != None, podzielniki_uzytkownika))
+        mianownik = sum(c.wspolczynnik for c in wiersze_niepuste)
+        srednia_wazona = sum(wiersze_niepuste[g].odczyt * wiersze_niepuste[g].wspolczynnik / mianownik for g in range(len(wiersze_niepuste)))
+        print(srednia_wazona)
+        funkcja = wynik_liczenia(wiersz_uzytkownika.nr_uzytkownika, wiersz_uzytkownika.numer_podz, wiersz_uzytkownika.wspolczynnik, srednia_wazona, wiersz_uzytkownika.uwagi)
+        lista_wynikow.append(funkcja)
     return lista_wynikow
 
 
